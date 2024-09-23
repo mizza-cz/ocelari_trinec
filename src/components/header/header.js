782aeb63063bdc72940b85cc507f397e2ee3f7c6
mizza-cz <mizza.cz666@gmail.com>
2024-09-23 12:03:18 +0200

    fix header menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,22 +1,34 @@
 headerNavOpenerClick();
 function headerNavOpenerClick() {
-  const bodyEl = document.querySelector('body');
-  const headerNavOpener = document.querySelector('.js-header__opener');
+  const bodyEl = document.querySelector("body");
+  const headerNavOpener = document.querySelector(".js-header__opener");
   if (!bodyEl || !headerNavOpener) {
-    return
+    return;
   }
-  headerNavOpener.addEventListener('click', function() {
-    if (!this.classList.contains('is-open')) {
-      bodyEl.classList.add('is-nav-open');
-      this.classList.add('is-open');
+  headerNavOpener.addEventListener("click", function () {
+    if (!this.classList.contains("is-open")) {
+      bodyEl.classList.add("is-nav-open");
+      this.classList.add("is-open");
     } else {
-      bodyEl.classList.remove('is-nav-open');
-      this.classList.remove('is-open');
+      bodyEl.classList.remove("is-nav-open");
+      this.classList.remove("is-open");
     }
   });
 }
-$(function(){
-   $('.navbar__menu-item').on('click', function () {
-      $(this).closest('.navbar__menu-list').toggleClass('active');
-    });
-})
+$(function () {
+  $(document).on("click", function (event) {
+    if (!$(event.target).closest(".navbar__menu-list").length) {
+      $(".navbar__menu-list").removeClass("active");
+    }
+  });
+
+  $(".navbar__menu-item").on("click", function (event) {
+    const $currentMenuList = $(this).closest(".navbar__menu-list");
+
+    $(".navbar__menu-list").not($currentMenuList).removeClass("active");
+
+    $currentMenuList.toggleClass("active");
+
+    event.stopPropagation();
+  });
+});
