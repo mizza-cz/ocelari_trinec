d47bb5f8dd63cf6a0e5c8c3c8bb9d0678472940b
mizza-cz <mizza.cz666@gmail.com>
2024-09-27 10:02:06 +0200

    test select

--- src/components/statistics-section/statistics-section.js
+++ src/components/statistics-section/statistics-section.js
@@ -1,31 +1,3 @@
-// function sortTable(table, sortClass) {
-//   const rows = Array.from(table.tBodies[0].rows);
-//   rows.sort(function (rowA, rowB) {
-//     const valueA = parseFloat(
-//       rowA.querySelector(sortClass).textContent.replace(",", ".")
-//     );
-//     const valueB = parseFloat(
-//       rowB.querySelector(sortClass).textContent.replace(",", ".")
-//     );
-//     return valueB - valueA;
-//   });
-
-//   rows.forEach(function (row) {
-//     table.tBodies[0].appendChild(row);
-//   });
-// }
-
-// window.onload = function () {
-//   // Goalkeeper Table - Sort by .sort-usp
-//   const goalkeeperTable = document.getElementById("goalkeeperTable");
-//   sortTable(goalkeeperTable, ".sort-usp");
-
-//   // Filter Table - Sort by .sortable-b
-//   const filterTables = document.querySelectorAll(".filter-b-table");
-//   filterTables.forEach(function (table) {
-//     sortTable(table, ".sortable-b");
-//   });
-// };
 function sortTable(table, sortClass) {
   const rows = Array.from(table.tBodies[0].rows);
   rows.sort(function (rowA, rowB) {
@@ -36,11 +8,9 @@ function sortTable(table, sortClass) {
       rowB.querySelector(sortClass).textContent.trim().replace(",", ".")
     );
 
-    // Compare based on the sortable column
     if (valueA !== valueB) {
-      return valueB - valueA; // Sort by the specified column descending
+      return valueB - valueA;
     } else {
-      // If values are equal, sort by goals ("G" column) descending
       const goalsA = parseFloat(
         rowA.querySelector(".number:nth-child(5)").textContent.trim()
       );
@@ -51,18 +21,15 @@ function sortTable(table, sortClass) {
     }
   });
 
-  // Re-append sorted rows to the table body
   rows.forEach(function (row) {
     table.tBodies[0].appendChild(row);
   });
 }
 
 window.onload = function () {
-  // Goalkeeper Table - Sort by .sort-usp
   const goalkeeperTable = document.getElementById("goalkeeperTable");
   sortTable(goalkeeperTable, ".sort-usp");
 
-  // Filter Table - Sort by .sortable-b
   const filterTables = document.querySelectorAll(".filter-b-table");
   filterTables.forEach(function (table) {
     sortTable(table, ".sortable-b");
--- src/js/custom/select2.js
+++ src/js/custom/select2.js
@@ -3,26 +3,6 @@ $(".js-select, .accreditation-select").select2({
   minimumResultsForSearch: -1,
 });
 
-function isMobileSafari() {
-  const ua = navigator.userAgent;
-  return (
-    /iP(ad|hone|od).+Version\/[\d.]+.*Safari/i.test(ua) &&
-    !/CriOS|Chrome/i.test(ua)
-  );
-}
-
-if (isMobileSafari()) {
-  $(document).on("select2:open", function (e) {
-    let select = $(e.target);
-
-    select.select2("close");
-
-    setTimeout(function () {
-      select.focus().click();
-    }, 100);
-  });
-}
-
 $(function () {
   //presmerovani pomoci selectu
   $(".js-select").change(function () {
