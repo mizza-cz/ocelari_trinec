641a8b3b6fdd6bce5ef7380f27a0f828a70b6200
mizza-cz <mizza.cz666@gmail.com>
2024-10-22 06:55:13 +0200

    add fix datatable/juniors

--- src/js/custom/tableSort.js
+++ src/js/custom/tableSort.js
@@ -1,25 +1,22 @@
 $(document).ready(function () {
-  // Функция для определения, нужно ли сортировать по дате
   function shouldSortDate() {
-    return $(".tableSort th.dateColumn").length > 0; // Проверка наличия класса dateColumn
+    return $(".tableSort th.dateColumn").length > 0;
   }
 
-  // Настройка сортировки по дате
   $.fn.dataTable.ext.type.order["date-pre"] = function (data) {
-    var dateParts = data.split(".").reverse(); // Разбиваем строку и переворачиваем
-    return new Date(dateParts[0], dateParts[1] - 1, dateParts[2]).getTime(); // Создаем объект даты
+    var dateParts = data.split(".").reverse();
+    return new Date(dateParts[0], dateParts[1] - 1, dateParts[2]).getTime();
   };
 
-  // Инициализация DataTable
   $(".tableSort").DataTable({
     paging: false,
     searching: false,
     columnDefs: [
       {
-        type: shouldSortDate() ? "date" : null, // Сортировка по дате только если класс есть
-        targets: 0, // Укажите индекс колонки с датой
+        type: shouldSortDate() ? "date" : null,
+        targets: 0,
       },
     ],
-    order: shouldSortDate() ? [[0, "desc"]] : [], // Сортировка по дате от самой последней к самой старой
+    order: shouldSortDate() ? [[0, "desc"]] : [],
   });
 });
